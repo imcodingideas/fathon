5926d43eda1e98ba0fe8e9f41979bf93ade5791f
Joseph Chambers <joseph@michael-chambers.com>
2017-10-22 21:52:55 -0700

    Changed the state to a functional component as state is not needed just yet.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../assets/css/search.css';
 import simbol from '../assets/images/searchSign.png';
 
-class Search extends Component {
-  render() {
-    return (
-      <div className="row searchContainer">
-        <div className="col s8">
-          <input className="searchInput" placeholder="Search..." />
-        </div>
-        <div className="col s1">
-          <button className="btn indigo darken-1 searchBtn">
-            <img src={simbol} className="searchBtnImg" />
-          </button>
-        </div>
+export default () => {
+  return (
+    <div className="row searchContainer">
+      <div className="col s8">
+        <input className="searchInput" placeholder="Search..." />
       </div>
-    );
-  }
-}
-
-export default Search;
+      <div className="col s1">
+        <button className="btn indigo darken-1 searchBtn">
+          <img src={simbol} className="searchBtnImg" />
+        </button>
+      </div>
+    </div>
+  );
+};
