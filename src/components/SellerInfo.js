d29b3e930e9bdb3fd2dc3ffb71ac1ba1072c1fdc
Joseph Chambers <joseph@michael-chambers.com>
2017-10-22 21:53:39 -0700

    No longer need a constuctor to have state.

--- src/components/SellerInfo.js
+++ src/components/SellerInfo.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import '../assets/css/sellerInfo.css';
 
 class SellerInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '5Fhantom',
-      address: 'Wallet Address',
-      id: '0x32Be343B94f860124dC4fEe278FDC2C102D88',
-      balance: '$5000'
-    };
-  }
+  state = {
+    name: '5Fhantom',
+    address: 'Wallet Address',
+    id: '0x32Be343B94f860124dC4fEe278FDC2C102D88',
+    balance: '$5000'
+  };
   render() {
     return (
       <div className="row ">
